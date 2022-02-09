a9dcadaee2c2a668dc0e56b193f2f8677c62ae92
tvogel <tvogel@student.42.fr>
2022-02-09 11:50:50 +0100

    refactor: add comment

---
+++ src/signal/handle_signal.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_signal.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 19:13:47 by tvogel            #+#    #+#             */
+/*   Updated: 2022/02/09 11:47:20 by tvogel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * Write \n then replace the line with a new prompt
+*/
+static void	handle_sigint(int sig)
+{
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", sig);
+	rl_redisplay();
+}
+
+/**
+ * This function is used to handle signals like ctrl-c and ctrl-\
+*/
+void	handle_signal(void)
+{
+	signal(SIGINT, &handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
+}
