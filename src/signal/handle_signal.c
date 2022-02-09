1b9b02ce274f65661d3aa5796d0adff75695ec56
tvogel <tvogel@student.42.fr>
2022-02-09 11:20:14 +0100

    fix: better behavior on ctrl-c

--- src/signal/handle_signal.c
+++ src/signal/handle_signal.c
@@ -6,7 +6,7 @@
 /*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 19:13:47 by tvogel            #+#    #+#             */
-/*   Updated: 2022/02/08 19:16:50 by tvogel           ###   ########.fr       */
+/*   Updated: 2022/02/09 11:17:44 by tvogel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,9 @@
 static void	handle_sigint(int sig)
 {
 	write(1, "\n", 1);
-	write_prompt(0);
-}
-
-static void	handle_sigquit(int sig)
-{
-	write(1, "exit\n", 5);
+	rl_on_new_line();
+	rl_replace_line("", sig);
+	rl_redisplay();
 }
 
 void	handle_signal(void)
