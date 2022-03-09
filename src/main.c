017ae11a794cfffce963014948d717e7c86df314
tvogel <tvogel@student.42.fr>
2022-03-09 13:53:18 +0100

    feat: parser is working with ">>"

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 21:45:04 by tvogel            #+#    #+#             */
-/*   Updated: 2022/03/04 21:36:53 by abiju-du         ###   ########.fr       */
+/*   Updated: 2022/03/09 13:31:55 by tvogel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,11 @@ int	main(int argc, char **argv, char **envp)
 	if (init(&c, envp) == FAILURE)
 		return (clean_exit(&c, ERR_INIT));
 	handle_signal();
-	c.command_line = readline("minishell$> ");
-	while (c.command_line != NULL)
+	while (1)
 	{
+		c.command_line = readline("minishell$> ");
+		if (c.command_line == NULL)
+			break ;
 		c.command_line = translator(&c, c.command_line);
 		add_history(c.command_line);
 		if (lexer(&c) != SUCCESS)
@@ -29,7 +31,6 @@ int	main(int argc, char **argv, char **envp)
 		else if (parser(&c) == SUCCESS)
 			exec(&c, envp);
 		clean_on_success(&c);
-		c.command_line = readline("minishell$> ");
 	}
 	printf("exit\n");
 	ft_lstclear(&c.env, free);
