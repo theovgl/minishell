3fa057c9f096334ebe2c005045394ba868a7f36d
ArnaudBD <abiju-du@student.42.fr>
2022-03-17 14:35:50 +0100

    refactor: init.c Norme OK

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abiju-du <abiju-du@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 16:04:51 by tvogel            #+#    #+#             */
-/*   Updated: 2022/03/15 16:55:12 by tvogel           ###   ########.fr       */
+/*   Updated: 2022/03/17 11:56:23 by abiju-du         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	init(t_config *c, char **ep)
 	c->tokens = NULL;
 	c->cmd_list = NULL;
 	c->nb_pipe = 0;
-	if(ft_env(c, ep) == FAILURE)
+	if (ft_env(c, ep) == FAILURE)
 		return (FAILURE);
 	return (SUCCESS);
 }
