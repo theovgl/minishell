57ee9fa033a086da2c559bfa5d967dba3fffe0d3
ArnaudBD <abiju-du@student.42.fr>
2022-03-18 13:54:05 +0100

    fix: works with a empty executable ; pipes: the error messages aren't mixed up any more

--- src/clean_exit.c
+++ src/clean_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   clean_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abiju-du <abiju-du@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:24:45 by tvogel            #+#    #+#             */
-/*   Updated: 2022/03/15 16:55:42 by tvogel           ###   ########.fr       */
+/*   Updated: 2022/03/18 12:17:23 by abiju-du         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,12 @@ void	clean(t_config *c)
 void	exit_failure(t_config *c, char *err_string, int is_errno)
 {
 	if (is_errno)
-		perror(err_string);
+	{
+		if (errno == ENOEXEC)
+			g_return = 0;
+		else
+			perror(err_string);
+	}	
 	else
 		ft_putstr_fd(err_string, 2);
 	clean(c);
--- src/utils/ft_putstr_fd.c
+++ src/utils/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abiju-du <abiju-du@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 12:00:55 by tvogel            #+#    #+#             */
-/*   Updated: 2022/03/03 15:51:56 by tvogel           ###   ########.fr       */
+/*   Updated: 2022/03/18 12:27:49 by abiju-du         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	i;
-
-	i = 0;
 	if (!s || !fd)
 		return ;
-	while (s[i] != '\0')
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	write (fd, s, strlen(s));
 }
